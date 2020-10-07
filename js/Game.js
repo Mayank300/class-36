28e6152c684249b8ca0b9e15ab2378c1d33e2163
Mayank <70268410+Mayank300@users.noreply.github.com>
2020-10-07 18:32:39 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,63 @@
+class Game {
+  constructor(){
+
+  }
+  play(){
+   form.hide();
+   textSize(40);
+   //fill("black");
+   text("Game Start",180,100);
+
+   if(allPlayers !== undefined){
+     var display_position = 190;
+
+    for(var plr in allPlayers){
+     if(plr === "player" + player.index){
+        fill("red");
+     }
+     else {
+       fill("black");
+     }
+     display_position += 20;
+     text(allPlayers[plr].name + ":" + allPlayers[plr].distance, 180,display_position);
+     }
+   }
+   if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance += 50;
+      player.update();
+   }
+  }
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form();
+      form.display();
+         
+      }
+      // player.getCount();
+      // form = new Form();
+      // form.display();
+    }
+  }
+
